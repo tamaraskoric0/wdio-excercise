f54816acc09bcbdb0a8d21b6cc6ce714e5308072
tamaraskoric0 <91868285+tamaraskoric0@users.noreply.github.com>
2021-12-09 19:55:52 +0100

    Update basic.e2e.js
    vjezbe- automatsko testiranje

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,22 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+describe('Shoutem Pricing page', () => {
+    it('Ucitavanje stranice Pricing', async () => {
+        await browser.url('https://shoutem.com/pricing/');
+
+        const h1 = $('h1');
+        await expect(h1).toHaveText('There are more ways to create apps with Shoutem');
+    });
+
+    it('Ucitavanje social landing stranica', async () => {
+        await browser.url('https://shoutem.com/apps/social-network/');
+
+        const button = await $('.btn=Build now');
+        await button.click();
+
+        const pageTitle = await $('.page-title');
+        await expect(pageTitle).toHaveText('Choose an app template')
+
+
+
     })
+
 })
